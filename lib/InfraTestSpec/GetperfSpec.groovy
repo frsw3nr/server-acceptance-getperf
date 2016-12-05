42f1fead81ee1b6d0bf758f1d30b9bbe5e7bf06e
Minoru Furusawa <minoru.furusawa@toshiba.co.jp>
2016-12-05 18:15:22 +0900

    tuning domain schenario

--- lib/InfraTestSpec/GetperfSpec.groovy
+++ lib/InfraTestSpec/GetperfSpec.groovy
@@ -77,4 +77,52 @@ class GetperfSpec extends LinuxSpecBase {
         test_item.devices(csv, headers)
         test_item.results(last_transfer_dates.toString())
     }
+
+    def domain(session, test_item) {
+        def lines_site_home = exec('site_home') {
+            def command = "grep -H home /home/psadmin/getperf/config/site/*"
+            run_ssh_command(session, command, 'site_home')
+        }
+        def site_homes = [:]
+        lines_site_home.eachLine {
+            ( it =~ /\/site\/(.+?)\.json:\s+"home":\s+"(.+?)"/).each {
+                m0, sitekey, site_home ->
+                site_homes[sitekey] = site_home
+            }
+        }
+        def last_transfer_dates = [:]
+        def csv = []
+        site_homes.each { sitekey, site_home ->
+            def log_file = "ls_node_${sitekey}"
+            def lines = exec(log_file) {
+                def paths = []
+                if (this.agent) {
+                    paths.add("${site_home}/node/*/${this.agent}/")
+                    def agent_uc = this.agent.toUpperCase()
+                    if (agent_uc != this.agent)
+                        paths.add("${site_home}/node/*/${agent_uc}/")
+                } else {
+                    paths.add("${site_home}/node/*/*/")
+                }
+                def command = "ls  -l -u -d --time-style=+%Y-%m-%d " + paths.join(' ')
+                command +=  ">> ${work_dir}/${log_file}"
+                session.execute command, ignoreError : true
+                session.get from: "${work_dir}/${log_file}", into: local_dir
+                new File("${local_dir}/${log_file}").text
+            }
+// drwxr-xr-x 4 psadmin cacti 4096 2016-12-05 /catai/peyok02/node/Linux/yqaj222/
+            lines.eachLine { line->
+                ( line =~ /(\d+-\d+-\d+) .+\/(.+?)\/(.+?)\/$/ ).each {
+                    m0, transfer_date, domain, node->
+                    csv << [sitekey, site_home, node, domain, transfer_date]
+                    last_transfer_dates['domain.' + domain] = transfer_date
+                }
+            }
+        }
+        def headers = ['Sitekey', 'SiteHome', 'Nodename', 'Domain', 'TransferDate']
+        test_item.devices(csv, headers)
+        last_transfer_dates['domain'] = last_transfer_dates.size()
+        test_item.results(last_transfer_dates)
+    }
+
 }
